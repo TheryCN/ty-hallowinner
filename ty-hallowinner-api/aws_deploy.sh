c1350df0f91373d0de042e98f2a49df076340e60
tcharasson <thery.charasson@gmail.com>
2019-11-08 11:36:40 +0100

    Add profiles and aws deployment

---
+++ ty-hallowinner-api/aws_deploy.sh
@@ -0,0 +1,9 @@
+# Generates ./elasticbeanstalk/config.yml
+# eb init ty-hallowinner-api --region eu-west-3
+
+eb setenv SPRING_PROFILES_ACTIVE=aws,h2
+eb setenv SERVER_PORT=5000
+
+mvn clean package spring-boot:repackage
+eb deploy
+eb status
